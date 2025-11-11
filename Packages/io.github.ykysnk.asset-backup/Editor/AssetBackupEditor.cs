6dce3534771c6c786c1565f893c2c306e2f4263c
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-12 02:54:07 +0800

    fix: upload files

---
+++ Packages/io.github.ykysnk.asset-backup/Editor/AssetBackupEditor.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using UnityEditor;
+
+namespace io.github.ykysnk.assetbackup.Editor
+{
+    public class AssetBackupEditor : EditorWindow
+    {
+        private const string MenuPath = "Assets/yky/Asset Backup #b";
+
+        [MenuItem(MenuPath, false)]
+        private static void Backup()
+        {
+            if (Selection.assetGUIDs.Length < 1) return;
+
+            var now = DateTime.Now;
+            var date = $"{now:yy-MM-dd}";
+
+            foreach (var guid in Selection.assetGUIDs)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var pathDir = Path.GetDirectoryName(path);
+                var ext = Path.GetExtension(path);
+                var newName = $"{Path.GetFileNameWithoutExtension(path)}_{date}";
+                var newPath = $"{pathDir}/{newName}{ext}";
+
+                AssetDatabase.CopyAsset(path, string.IsNullOrEmpty(AssetDatabase.AssetPathToGUID(newPath))
+                    ? newPath
+                    : GetNewPathUntilNotExist(pathDir, newName, ext));
+            }
+        }
+
+        private static string GetNewPathUntilNotExist(string pathDir, string name, string ext)
+        {
+            var count = 1;
+            string newPath;
+
+            while (!string.IsNullOrEmpty(AssetDatabase.AssetPathToGUID(newPath = $"{pathDir}/{name}.{count:000}{ext}")))
+                count++;
+
+            return newPath;
+        }
+    }
+}
---
+++ Packages/io.github.ykysnk.asset-backup/README.md
@@ -0,0 +1,1 @@
+# io.github.ykysnk.asset-backup by vycpjkfhweiqoyszx6gj
